ad0e9fe7dac504653abf5cae9a2effebc1675b69
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-04 22:06:22 -0500

    refactor Weather resources

--- AspNet7WebApiAuth/Controllers/WeatherForecastController.cs
+++ AspNet7WebApiAuth/Controllers/WeatherForecastController.cs
@@ -18,16 +18,11 @@ namespace AspNet7WebApiAuth.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(Summaries);
         }
     }
 }
