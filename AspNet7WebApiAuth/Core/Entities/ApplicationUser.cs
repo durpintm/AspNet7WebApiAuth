6ebc3039a199bd44075f7101d874f082d0929dcd
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-05 16:53:09 -0500

    refactor entire api to use ApplicationUser

---
+++ AspNet7WebApiAuth/Core/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNet7WebApiAuth.Core.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- AspNet7WebApiAuth/Program.cs
+++ AspNet7WebApiAuth/Program.cs
@@ -1,4 +1,5 @@
 using AspNet7WebApiAuth.Core.DbContext;
+using AspNet7WebApiAuth.Core.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -20,7 +21,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 // Add Identity
 builder.Services
-    .AddIdentity<IdentityUser, IdentityRole>()
+    .AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
