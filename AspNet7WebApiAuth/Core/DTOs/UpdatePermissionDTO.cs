e3b8e4b961d86002954d302900de6b99f9daea1b
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-04 23:50:45 -0500

    Added DTOs and Static Roles

---
+++ AspNet7WebApiAuth/Core/DTOs/UpdatePermissionDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNet7WebApiAuth.Core.DTOs
+{
+    public class UpdatePermissionDTO
+    {
+        [Required(ErrorMessage = "UserName is Required")]
+        public string UserName { get; set; }
+
+    }
+}
